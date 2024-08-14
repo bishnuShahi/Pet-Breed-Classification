40f999026ad2186437a947291da433e5b6245ce2
bishnuShahi <bishnushahi2003@gmail.com>
2024-08-14 19:19:19 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ python3 -m pip install -r requirements.txt
 
 echo "Building Tailwind CSS..."
 NODE_ENV=production python3 manage.py tailwind build
+
+python3 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+
+python3 manage.py collectstatic --noinput
