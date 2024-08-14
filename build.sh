c795167f669da571177c0ea3fc91ed1948e6da1f
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-08-14 17:23:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 echo "Building project packages..."
-npm install
 python3 -m pip install -r requirements.txt
 
 echo "Building Tailwind CSS..."
 NODE_ENV=production python3 manage.py tailwind build
-
-echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
-
-echo "Listing static files..."
-ls -R staticfiles/
