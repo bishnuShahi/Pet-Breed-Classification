e9972b4b5b3ef96df4b54bde79684bcd0c0bc198
bishnuShahi <bishnushahi2003@gmail.com>
2024-08-18 16:02:59 +0530

    Fixing static files issue

--- Project/settings.py
+++ Project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,3 @@
 pip install -r requirements.txt
-python3 manage.py migrate
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
