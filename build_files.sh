a599dbce22eda5482daa49a204945b52c9ca4e21
bishnuShahi <bishnushahi2003@gmail.com>
2024-08-21 17:59:02 +0530

    Testing new fix

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
-pip install -r requirements.txt
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+python3.12 pip install -U pip
+
+# Install dependencies
+python3.12 -m pip install -r requirements.txt
+
+# Make migrations
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+#Install whitenoise
+python3.12 manage.py whitenoise
+
+python3.12 manage.py collectstatic --noinput --clear
